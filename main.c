da54f384d9fd6bd582ea1a302bf5663a4ee332e4
Kenshin Urashima <kenshin.develop@gmail.com>
2019-06-29 00:05:56 -0400

    adaptacion

--- main.c
+++ main.c
@@ -1,17 +1,137 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "headers/map.h"
+#include <string.h>
+
+#ifndef __cplusplus
+
+typedef enum bool {
+    false = 0,
+    true
+}bool;
+
+#endif //__cplusplus
+
+/**************************************************************
+ *-----------------------ESTRUCTURAS---------------------------
+ **************************************************************/
+
+typedef struct Point {
+    int x;
+    int y;
+}Point;
+
+typedef struct List {
+    Point point;
+    struct List *prox;
+}List;
+
+typedef struct Map {
+    int width;
+    int height;
+    int maxSteps;
+    int numEnemies;
+    char **matrix;
+}Map;
+
+
+/**************************************************************
+ *-----------------------PROTOTIPOS----------------------------
+ **************************************************************/
+
+Map NewMap(const char *path);
+
+void DeleteMap(Map *map);
+
+static bool __InitMatrixMap(Map *map, const char *path);
+
+static void __PrintMatrixMap(const Map *map);
+
+/*************************************************************
+*-----------------------FUNC--MAIN----------------------------
+**************************************************************/
+
 
 int main(int argc, char *argv[]) {
-    Map map = NewMap("resources/maps/map.txt");
+    const char *path = "entrada.txt";
+    if (argc == 2)
+        path = argv[1];
 
-    printf("steps: %d\n", map.steps);
-    printf("width: %d\n", map.width);
-    printf("height: %d\n", map.height);
+    Map map = NewMap(path);
 
-    PrintMap(&map);
+    __PrintMatrixMap(&map);
 
     DeleteMap(&map);
 
     return EXIT_SUCCESS;
 }
+
+/**************************************************************
+ *-----------------------IMPLEMENTACION------------------------
+ **************************************************************/
+ Map NewMap(const char *path) {
+     Map map = {0};
+     map.matrix = NULL;
+     __InitMatrixMap(&map, path);
+     return map;
+ }
+
+ void DeleteMap(Map *map) {
+     for (int i=0; i < map->height; i++) {
+         free(map->matrix);
+         map->matrix = NULL;
+     }
+
+     if (map->matrix != NULL) {
+         free(map->matrix);
+         map->matrix = NULL;
+     }
+ }
+
+ static bool __InitMatrixMap(Map *map, const char *path) {
+     FILE *file = fopen(path, "r");
+     if (file == NULL) {
+         printf("No se ha podido abrir el fichero.\n");
+         return false;
+     }
+
+     char *row = (char *)malloc(sizeof(char)*128);
+     int indexRow = -1;
+     while (feof(file) == 0) {
+         fgets(row, 128, file);
+         if (indexRow == -1) {
+             char *auxRow = row;
+
+             char *token = strtok(auxRow, " ");
+             map->maxSteps = atoi(token);
+
+             token = strtok(NULL, " ");
+             map->width = atoi(token);
+
+             token = strtok(NULL, " ");
+             map->height = atoi(token);
+         }
+         else if (indexRow > -1) {
+         }
+
+         printf("%s ", row);
+         indexRow++;
+     }
+
+     if (row != NULL) {
+         free(row);
+         row = NULL;
+     }
+     fclose(file);
+     return true;
+ }
+
+ static void __PrintMatrixMap(const Map *map) {
+     printf("\n");
+     for (int i=0; i < map->height; i++) {
+         for (int j=0; j < map->width; j++) {
+             printf("%c ", map->matrix[i][j]);
+         }
+         printf("\n");
+     }
+     printf("\n");
+ }
--- run.bat
+++
@@ -1,4 +0,0 @@
-gcc -c main.c -o main.o -std=c11
-gcc -c src/map.c -o map.o -std=c11
-gcc main.o map.o -o bomberman -std=c11
-del *.o
